7dff31585f5692af94b316d8856514ec4057b61f
Alioun Diagne <diagne@hm.edu>
2016-04-21 11:03:33 +0200

    A B and C got threads

--- src/edu/hm/cs/rs/arch/a02_waitnotify/OperationsSequential.java
+++ src/edu/hm/cs/rs/arch/a02_waitnotify/OperationsSequential.java
@@ -5,15 +5,24 @@ import static edu.hm.cs.rs.arch.a02_waitnotify.Operation.*;
 public class OperationsSequential {
     public static void main(String... args) {
         init(args);
-        A1.exec();
-        A2.exec();
-        A3.exec();
-        B1.exec();
-        B2.exec();
-        B3.exec();
-        C1.exec();
-        C2.exec();
-        C3.exec();
+        Runnable runAs = () -> {
+            A1.exec();
+            A2.exec();
+            A3.exec();
+        };
+
+        Runnable runBs = () -> {
+            B1.exec();
+            B2.exec();
+            B3.exec();
+        };
+
+        Runnable runCs = () -> {
+            C1.exec();
+            C2.exec();
+            C3.exec();
+        };
+
         System.out.println("complete");
     }
 }
