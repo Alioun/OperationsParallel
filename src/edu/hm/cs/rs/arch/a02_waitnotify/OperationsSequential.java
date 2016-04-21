acc58d6e0381f2aebbbd4ad0c3fab3ee37422e79
Alioun Diagne <diagne@hm.edu>
2016-04-21 12:37:02 +0200

    First sketch of an Idea

--- src/edu/hm/cs/rs/arch/a02_waitnotify/OperationsSequential.java
+++ src/edu/hm/cs/rs/arch/a02_waitnotify/OperationsSequential.java
@@ -1,24 +1,66 @@
 package edu.hm.cs.rs.arch.a02_waitnotify;
 
-import static edu.hm.cs.rs.arch.a02_waitnotify.Operation.*;
+import static edu.hm.cs.rs.arch.a02_wai tnotify.Operation.*;
 
 public class OperationsSequential {
+    private int firstCounter;
+    private int secondCounter;
+
+    public OperationsSequential(){
+        firstCounter = 0;
+        secondCounter = 0;
+    }
+
+    private void setFirstCounter(int data){
+        firstCounter = data;
+    }
+
+    private int getFirstCounter(){
+        return firstCounter;
+    }
+
+    private void setSecondCounter(int data){
+        secondCounter = data;
+    }
+
+    private int getSecondCounter(){
+        return secondCounter;
+    }
+
+
     public static void main(String... args) {
         init(args);
+
+        OperationsSequential operationsSequential = new OperationsSequential();
+        Object monitor = new Object();
+
         Runnable runAs = () -> {
             A1.exec();
+            synchronized (monitor){
+            operationsSequential.setFirstCounter(operationsSequential.getSecondCounter()+1);
+                monitor.notifyAll();
+            }
             A2.exec();
             A3.exec();
         };
 
         Runnable runBs = () -> {
             B1.exec();
+            synchronized (monitor){
+                operationsSequential.setFirstCounter(operationsSequential.getSecondCounter()+1);
+                monitor.notifyAll();
+            }
             B2.exec();
             B3.exec();
         };
 
         Runnable runCs = () -> {
             C1.exec();
+            synchronized (monitor){
+            if(operationsSequential.getFirstCounter()==2){
+                //do stuff
+            }
+            }
             C2.exec();
             C3.exec();
         };
